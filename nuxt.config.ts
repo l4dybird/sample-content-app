35363010e1be96a93445d8d1abe38ae2f65e3c60
Yuki <k.yuuki1997@gmail.com>
2022-05-19 20:55:55 +0900

    :tada: created project

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  content: {
+    // base: '/_content',
+    // watch: true
+  },
+  modules: ['@nuxt/content'],
+})
---
+++ pages/[...slug].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+const { data } = await useAsyncData('page-data', () => queryContent().find());
+const page = data.value.filter((x) => x._path === route.fullPath)[0];
+</script>
+
+
+<template>
+  <main>
+    <ContentRenderer :value="page">
+      <template #empty>
+        <p>No content found.</p>
+      </template>
+    </ContentRenderer>
+  </main>
+</template>
