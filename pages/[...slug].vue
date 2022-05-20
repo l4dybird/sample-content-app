544bd1c7f5a2033a0c392c0d718d248b3969944c
Yuki <k.yuuki1997@gmail.com>
2022-05-20 21:47:28 +0900

    :recycle: ContentDoc

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,5 @@
-<script setup lang="ts">
-const route = useRoute();
-const { data } = await useAsyncData('page-data', () => queryContent().find());
-const page = data.value.filter((x) => x._path === route.fullPath)[0];
-</script>
-
-
 <template>
   <main>
-    <ContentDoc :value="page"/>
+    <ContentDoc />
   </main>
 </template>
